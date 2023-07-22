e8fa3f71f859e43e8ec681356025ad9a6ae5dd58
mn.ikhwan <72797830+mnikhwan@users.noreply.github.com>
2023-07-22 12:07:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
